3641e78e22e546a07f8238415e2aed241c2a3345
faskaa <fazilnb@code.edu.az>
2023-08-25 13:34:25 +0400

    first commit initizailed

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var time = Console.ReadLine();
+            var newTime = DateTime.ParseExact(time, "dd/MM/yyyy", null).AddDays(30).AddHours(6).AddMinutes(17);
+            Console.WriteLine(newTime);
+
+
+
+
+
+
+
+        }
+    }
+}
